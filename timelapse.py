002271d90cbd075e789083eca59c8ae1a67ce1cb
Jeff Heidel <jeh295@cornell.edu>
2014-12-29 22:11:00 -0800

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -1,47 +1,59 @@
-
-import sys
-import os
+"""Merges a time lapse sequence into a blended sequence."""
+import argparse
 import numpy
+import os
 from PIL import Image
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('timelapse_dir', help='Path to directory containing timelapse images.')
+parser.add_argument('output_dir', help='Directory for output merged sequence. Will be created.')
+parser.add_argument('--darken', action='store_true', help='Enable darken mode.')
+parser.add_argument('--skip', nargs='?', const=1, type=int,
+                    help='Use one out of every SKIP images.')
+args = parser.parse_args()
 
 
-def MergeImages(img1, img2, min=True):
+def MergeImages(img1, img2, lighten=True):
   assert img1.size == img2.size
   a1 = numpy.asarray(img1)
   a2 = numpy.asarray(img2)
-  if min:
-    ao = numpy.minimum(a1, a2)
-  else:
+  if lighten:
     ao = numpy.maximum(a1, a2)
-  return Image.fromarray(a0, mode=img1.mode)
+  else:
+    ao = numpy.minimum(a1, a2)
+  return Image.fromarray(ao, mode=img1.mode)
 
 
 def Main():
   print '*** Super Timelapse Merge Script ***'
   
-  dir_path = sys.argv[1]
-  filenames = os.listdir(dir_path)
+  filenames = os.listdir(args.timelapse_dir)
   filenames.sort()
   
-  out_dir = sys.argv[2]
-  os.makedirs(out_dir)
+  if args.skip != 1:
+    print 'Filtering list. Using 1 out of every %d images.' % args.skip
+    filenames[:] = filenames[::args.skip]
+
+  if args.darken:
+    print 'Using darken mode.'
+    
+  os.makedirs(args.output_dir)
   
   history_img = None
-  c = 0
   
-  for fname in filenames:
-    fpath = os.path.join(dir_path, fname)
+  for c, fname in enumerate(filenames):
+    fpath = os.path.join(args.timelapse_dir, fname)
     print 'Processing %s (%d / %d)' % (fpath, c + 1, len(filenames))
     if history_img is None:
       img_out = Image.open(fpath)
     else:
       img_new = Image.open(fpath)
-      img_out = MergeImages(history_img, img_new)
+      img_out = MergeImages(history_img, img_new, lighten=not args.darken)
     
-    img_out.save(os.path.join(out_dir, 'output%05d.jpg' % c),
+    img_out.save(os.path.join(args.output_dir, 'output%05d.jpg' % c),
                  'JPEG', quality=97)
     history_img = img_out
-    c += 1
 
   print 'DONE.'
 
